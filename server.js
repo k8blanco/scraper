b6860fb41137e3bcd383ce835a552a227eb4f450
k8blanco <katiewhite303@gmail.com>
2019-03-31 18:42:21 -0600

    initial file setup

---
+++ server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var axios = require("axios");
+
