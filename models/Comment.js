c7bc0a0e7ac13ad1aa01c83341cc4648da3ee158
k8blanco <katiewhite303@gmail.com>
2019-04-03 09:48:36 -0600

    unique validator for articles in place and functional

--- models/Comment.js
+++ models/Comment.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
+//Unique validator to prevent duplicate db entries
+const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 
+
 const CommentSchema = new Schema({
     title: String,
-    body: String
+    body: String,
 });
+
+//Prevent duplicate comments in DB
+CommentSchema.plugin(uniqueValidator);
 
 const Comment = mongoose.model("Comment", CommentSchema);
 
